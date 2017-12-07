a5c82eea8f9f3825c56e02da049b233b2a2f1852
apple <apple@apples-air.lan>
2017-12-07 18:10:25 -0500

    saved aircraft info

--- WayPoints/SettingsAircraftTableViewController.swift
+++ WayPoints/SettingsAircraftTableViewController.swift
@@ -8,14 +8,45 @@
 
 import UIKit
 
-class SettingsAircraftTableViewController: UITableViewController {
+class SettingsAircraftTableViewController: UITableViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var aircraftRegTextField: UITextField!
+    @IBOutlet weak var aircraftTypeTextField: UITextField!
+    @IBAction func setAircraftReg(_ sender: UITextField) {
+        print("Registriation will set")
+        defaults.set(sender.text!, forKey: "defaultAircraftRegistration")
+        defaults.synchronize()
+    }
+    @IBAction func setAircraftType(_ sender: UITextField) {
+        print("AC Type will set")
+        defaults.set(sender.text!, forKey: "defaultAircraftType")
+        defaults.synchronize()
+    }
+    
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if let text = textField.text {
+            return text.count < 6
+        }
+        return true
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.estimatedRowHeight=50
-        tableView.rowHeight = UITableViewAutomaticDimension
+        self.tableView.estimatedRowHeight=50
+        self.tableView.rowHeight = UITableViewAutomaticDimension
         self.title = "Default Aircraft"
+        self.tableView.tableFooterView = UIView()
+        self.aircraftRegTextField.delegate=self
+        self.aircraftTypeTextField.delegate=self
+        if let reg=defaults.string(forKey: "defaultAircraftRegistration"), let acType = defaults.string(forKey: "defaultAircraftType") {
+            aircraftRegTextField.text = reg
+            aircraftTypeTextField.text = acType
+        }
+        
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
