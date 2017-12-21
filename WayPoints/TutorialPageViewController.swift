356dfbf18746d69fbf4750a45785edb5bb1d1f0c
apple <apple@apples-air.lan>
2017-12-21 13:53:27 -0500

    added tutorial as page view controller

---
+++ WayPoints/TutorialPageViewController.swift
@@ -0,0 +1,119 @@
+//
+//  TutorialPageViewController.swift
+//  WayPoints
+//
+//  Created by apple on 12/21/17.
+//  Copyright © 2017 jel enterprises. All rights reserved.
+//
+
+import UIKit
+
+class TutorialPageViewController: UIPageViewController, UIPageViewControllerDelegate {
+
+    var pageControl = UIPageControl()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.dataSource=self
+        self.delegate=self
+        configurePageControl()
+        
+        
+        
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
+    }
+
+    private(set) lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newTutorialViewController("1"),
+                self.newTutorialViewController("2"),
+                self.newTutorialViewController("3"),
+                self.newTutorialViewController("4"),
+                self.newTutorialViewController("5")]
+    }()
+    
+    private func newTutorialViewController(_ number: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil) .
+            instantiateViewController(withIdentifier: "tutorial\(number)")
+    }
+    
+    func configurePageControl() {
+        let tabBarHeight = self.tabBarController!.tabBar.frame.height
+        pageControl = UIPageControl(frame: CGRect(x: 0,y: UIScreen.main.bounds.maxY - tabBarHeight - 50,width: UIScreen.main.bounds.width,height: 50))
+        self.pageControl.numberOfPages = orderedViewControllers.count
+        self.pageControl.currentPage = 0
+        self.pageControl.alpha = 0.5
+        self.pageControl.tintColor = UIColor.blue
+        self.pageControl.pageIndicatorTintColor = UIColor.gray
+        self.pageControl.currentPageIndicatorTintColor = UIColor.blue
+        self.view.addSubview(pageControl)
+    }
+    
+    // MARK: Delegate functions
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        let pageContentViewController = pageViewController.viewControllers![0]
+        self.pageControl.currentPage = orderedViewControllers.index(of: pageContentViewController)!
+    }
+
+}
+
+// MARK: UIPageViewControllerDataSource
+
+extension TutorialPageViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        guard orderedViewControllersCount != nextIndex else {
+            return nil
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+    
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return orderedViewControllers.count
+    }
+    
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+        guard let firstViewController = viewControllers?.first,
+            let firstViewControllerIndex = orderedViewControllers.index(of: firstViewController) else {
+                return 0
+        }
+        
+        return firstViewControllerIndex
+    }
+   
+    
+}
